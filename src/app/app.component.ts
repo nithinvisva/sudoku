70156bbdb20df2e14225ceec9ffb88e7071f2333
nithinvisva <visvanic@gmailcom>
2022-07-30 14:04:55 +0530

    user interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import * as _ from 'lodash';
+import { SUDOKU } from './constants/constants';
 import { Solution, Solved } from './interface';
 
 @Component({
@@ -7,35 +8,28 @@ import { Solution, Solved } from './interface';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  _=_;
+export class AppComponent {
+
   title = 'sudoku';
   array: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8];
-  allowedNumbersInCell:number[] =[1,2,3,4,5,6,7,8,9];
-  sudoku: Array<number[]> = [
-    [5,0,0,0,0,0,0,0,9], 
-    [0,4,0,0,1,5,0,8,0], 
-    [0,0,7,0,0,3,4,5,2], 
-    [2,1,0,7,6,0,0,3,0], 
-    [0,0,4,0,9,8,2,6,1], 
-    [0,0,0,0,3,2,0,0,4], 
-    [9,0,6,0,7,1,0,0,3], 
-    [8,0,1,0,4,0,0,2,7], 
-    [0,0,3,0,0,0,0,9,8]];
-  solution: Array<Solution> =[]
+  allowedNumbersInCell: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  sudoku: Array<number[]> = _.cloneDeep(SUDOKU.empty)
+  isSolved: boolean = false;
+  isGivenCorrect: boolean = true;
+  solution: Array<Solution> = []
+  prevSolution: Array<Solution> = []
 
-  constructor(){
-    this.sudokuSolver();
+  constructor() {
   }
-  sudokuSolver(){
-    this.sudoku.map((data, i)=>{
-      data.map((box,j)=>{
-        const sol ={
+  sudokuSolver() {
+    this.sudoku.map((data, i) => {
+      data.map((box, j) => {
+        const sol = {
           'value': box,
-          'possibleValues':[],
+          'possibleValues': [],
           'i': i,
           'j': j,
-          'blockId': this.getBlockId(i,j),
+          'blockId': this.getBlockId(i, j),
           'isSolved': box == 0 ? false : true
         }
         this.solution.push(sol);
@@ -43,44 +37,44 @@ export class AppComponent  {
     })
     this.updatePossibleValues();
   }
-  getBlockId(i: number,j:number): any {
-    switch(i){
+  getBlockId(i: number, j: number): any {
+    switch (i) {
       case 0:
       case 1:
       case 2:
-        if(j<3){
+        if (j < 3) {
           return 0
         }
-        else if(j <6){
+        else if (j < 6) {
           return 1
         }
-        else if(j <9){
-         return 2
+        else if (j < 9) {
+          return 2
         }
         break;
       case 3:
       case 4:
       case 5:
-        if(j<3){
-         return 3
+        if (j < 3) {
+          return 3
         }
-        else if(j <6){
+        else if (j < 6) {
           return 4
         }
-        else if(j <9){
+        else if (j < 9) {
           return 5
         }
         break;
       case 6:
       case 7:
       case 8:
-        if(j<3){
+        if (j < 3) {
           return 6
         }
-        else if(j <6){
+        else if (j < 6) {
           return 7
         }
-        else if(j <9){
+        else if (j < 9) {
           return 8
         }
         break;
@@ -88,55 +82,76 @@ export class AppComponent  {
         return 10
     }
   }
-  updatePossibleValues(){
-    this.solution.map((data)=>{
-      if( data.value ==0){
-      const possibleFromColumn:number[]= this.checkColumnRow(data.i,true);
-      const possibleFromRow:number[]= this.checkColumnRow(data.j,false);
-      const possibleFromBox:number[] = this.checkBox(data.blockId);
-       data.possibleValues = this.removeCommon(possibleFromColumn,possibleFromRow,possibleFromBox)
+  updatePossibleValues() {
+    this.solution.map((data) => {
+      if (data.value == 0) {
+        const possibleFromColumn: number[] = this.checkColumnRow(data.i, true);
+        const possibleFromRow: number[] = this.checkColumnRow(data.j, false);
+        const possibleFromBox: number[] = this.checkBox(data.blockId);
+        data.possibleValues = this.removeCommon(possibleFromColumn, possibleFromRow, possibleFromBox)
       }
-      if(data.possibleValues.length == 1){
+      if (data.possibleValues.length == 1) {
         data.value = data.possibleValues[0];
         this.sudoku[data.i][data.j] = data.possibleValues[0]
       }
     })
-    const solved: any= _.groupBy(this.solution, 'isSolved' )
-    if(solved.false){
-      this.solution =[] as Solution[]
-      this.sudokuSolver();
+    const solved: any = _.groupBy(this.solution, 'isSolved')
+    if (solved.false) {
+      if (this.prevSolution && _.isEqual(this.prevSolution, this.solution)) {
+        this.isGivenCorrect = false;
+      }
+      if (this.isGivenCorrect) {
+        this.prevSolution = _.cloneDeep(this.solution)
+        this.solution = [] as Solution[]
+        this.sudokuSolver();
+      }
+    } else {
+      this.isSolved = true;
     }
   }
-  checkColumnRow(index:number,isColumn:boolean){
-    const arr =[];
-    for(let i of this.array){
-      if(isColumn ? this.sudoku[index][i] != 0 : this.sudoku[i][index] != 0) {
+  checkColumnRow(index: number, isColumn: boolean) {
+    const arr = [];
+    for (let i of this.array) {
+      if (isColumn ? this.sudoku[index][i] != 0 : this.sudoku[i][index] != 0) {
         arr.push(isColumn ? this.sudoku[index][i] : this.sudoku[i][index])
       }
     }
-    return this.compArray(this.allowedNumbersInCell,arr)
+    return this.compArray(this.allowedNumbersInCell, arr)
   }
-  compArray (first: number[], second :number[] ):number[] {
-    return first.filter(item =>{
+  compArray(first: number[], second: number[]): number[] {
+    return first.filter(item => {
       return !(_.includes(second, item))
     })
- };
- removeCommon (first: number[], second :number[], third:number[] ):number[] {
-  const spread= _.union(_.union(first, second), third);
-  return spread.filter(item =>{
-    return (first.includes(item) && second.includes(item) && third.includes(item)) 
-  })
-};
- checkBox(blockId:number){
-  const arr:number[] =[]
-   const block = _.groupBy(this.solution, 'blockId');
-   block[blockId].map((data)=>{
-    if(data.value != 0){
-      arr.push(data.value)
-    }
-   })
-   return this.compArray(this.allowedNumbersInCell,arr)
- }
-
+  };
+  removeCommon(first: number[], second: number[], third: number[]): number[] {
+    const spread = _.union(_.union(first, second), third);
+    return spread.filter(item => {
+      return (first.includes(item) && second.includes(item) && third.includes(item))
+    })
+  };
+  checkBox(blockId: number) {
+    const arr: number[] = []
+    const block = _.groupBy(this.solution, 'blockId');
+    block[blockId].map((data) => {
+      if (data.value != 0) {
+        arr.push(data.value)
+      }
+    })
+    return this.compArray(this.allowedNumbersInCell, arr)
+  }
+  reloadSudoku() {
+    this.resetFields();
+    this.sudoku = SUDOKU.empty;
+  }
+  loadExample() {
+    this.resetFields();
+    this.sudoku = _.cloneDeep(SUDOKU.example);
+  }
+  resetFields() {
+    this.isSolved = false;
+    this.isGivenCorrect = true;
+    this.solution = [];
+    this.prevSolution = [];
+  }
 }
 
